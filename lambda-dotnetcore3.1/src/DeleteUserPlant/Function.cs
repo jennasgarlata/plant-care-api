9cc75fac82871a299def9c787545c60eeb6e72f0
Jenna Sgarlata <jenna.sgarlata@gmail.com>
2022-11-16 12:41:51 -0600

    delete user plant endpoint

---
+++ lambda-dotnetcore3.1/src/DeleteUserPlant/Function.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace DeleteUserPlant
+{
+
+    public class Function
+    {
+
+        private static readonly HttpClient client = new HttpClient();
+
+        private static async Task<string> GetCallingIP()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Add("User-Agent", "AWS Lambda .Net Client");
+
+            var msg = await client.GetStringAsync("http://checkip.amazonaws.com/").ConfigureAwait(continueOnCapturedContext:false);
+
+            return msg.Replace("\n","");
+        }
+
+        public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
+        {
+            var location = await GetCallingIP();
+            var userPlantId = "0";
+            if (apigProxyEvent.QueryStringParameters.ContainsKey("userPlantId"))
+            {
+                if (!apigProxyEvent.QueryStringParameters.TryGetValue("userPlantId", out userPlantId)){
+                    return new APIGatewayProxyResponse
+                    {
+                        Body = JsonSerializer.Serialize("Could not find parameter."),
+                        StatusCode = 400,
+                        Headers = new Dictionary<string, string> { 
+                            { "Content-Type", "application/json" },
+                            { "Access-Control-Allow-Headers", "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token" },
+                            { "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,HEAD,OPTIONS" },
+                            { "Access-Control-Allow-Credentials", "true" },
+                            { "Access-Control-Allow-Origin", "*" },
+                            { "X-Requested-With", "*" }
+                        }
+                    };
+                }
+            }
+
+            var dbCon = DbConnection.Instance();
+            dbCon.Server = "plant-care-app-db.ckxkonakdsgz.us-east-1.rds.amazonaws.com";
+            dbCon.DatabaseName = "plant_care_app";
+            dbCon.UserName = "admin";
+            dbCon.Password = "";
+            dbCon.Port = "3306";
+            try {
+                if (dbCon.IsConnect())
+                {
+                    string query = "USE plant_care_app;" +
+                        "DELETE FROM tbl_user_plant WHERE user_plant_id = '" + userPlantId +  "';"; 
+                    var cmd = new MySqlCommand(query, dbCon.Connection);
+                    // cmd.Parameters.AddWithValue("@user_plant_id", userPlantId);
+                    // cmd.Prepare();
+                    cmd.ExecuteNonQuery();
+                }
+            } finally {
+                dbCon.Close();
+            }
+            
+
+            return new APIGatewayProxyResponse
+            {
+                Body = JsonSerializer.Serialize("Sucessfully deleted plant with userPlantId: " + userPlantId),
+                StatusCode = 200,
+                Headers = new Dictionary<string, string> { 
+                    { "Content-Type", "application/json" },
+                    { "Access-Control-Allow-Headers", "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token" },
+                    { "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,HEAD,OPTIONS" },
+                    { "Access-Control-Allow-Credentials", "true" },
+                    { "Access-Control-Allow-Origin", "*" },
+                    { "X-Requested-With", "*" }
+                }
+            };
+        }
+
+
+    }
+}
